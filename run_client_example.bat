05203eac3beba9bcc165617ce1b62c06d60579a9
CryhaSV2010 <146880899+CryhaSV2010@users.noreply.github.com>
2026-01-15 23:07:51 +0300

    Delete run_client_example.bat

--- run_client_example.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running Smart Payload Splitter Client...
-echo.
-echo Example: Sending test payload to local server
-echo.
-python client/payload_fragmenter.py test_payload.txt -u http://localhost:5000 --assemble
-pause
